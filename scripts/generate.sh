b812ac3d6f0b880dab43a1df89f698574f6d87d7
Benoit Eudier <benoit.eudier@gmail.com>
2018-10-08 11:13:15 +0900

    Continue script to generate message classes

---
+++ scripts/generate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DESTINATION="../src/common/"
+GENERATED_FOLDER="generated"
+
+rm -r ${GENERATED_FOLDER}
+mkdir -p ${GENERATED_FOLDER}/messages
+
+# First, generate the files
+pipenv run python generate.py messages.yaml
+
+# Run clang-format on the folder
+for filename in ${GENERATED_FOLDER}/*.h; do
+    [ -e "$filename" ] || continue
+    clang-format -i ${filename}
+done
+
+for filename in ${GENERATED_FOLDER}/messages/*.h; do
+    [ -e "$filename" ] || continue
+    clang-format -i ${filename}
+done
+
+# Move them !
+mv ${GENERATED_FOLDER}/* $DESTINATION
